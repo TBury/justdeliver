d1173c8c1bf918be87c71a98cf591be91588d354
Tomasz Bury <tomasz.bury@onet.eu>
2022-02-07 14:19:47 +0100

    Create selecting delivery upload method template & view

--- justdeliver/system/urls.py
+++ justdeliver/system/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard/', views.dashboard)
+    path('dashboard/', views.dashboard),
+    path('Deliveries/select-delivery-method/', views.select_delivery_adding_mode),
 ]
--- justdeliver/system/views.py
+++ justdeliver/system/views.py
@@ -6,3 +6,7 @@ def dashboard(request):
     driver = Driver.get_driver_by_user_profile(request.user)
     info = driver.get_driver_info()
     return render(request, "dashboard.html", info)
+
+
+def select_delivery_adding_mode(request):
+    return render(request, "choose_delivery_method.html")
